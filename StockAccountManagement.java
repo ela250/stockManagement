a472ed161706b3f570b446aac5fe76b21bfb67a0
elamparethi <elamparethi02a2gmail.com>
2023-02-17 07:54:49 +0530

    Added:StockAccountManagement java Program

---
+++ StockAccountManagement.java
@@ -0,0 +1,19 @@
+package com.stock;
+
+public class StockAccountManagement {
+    public void addStocks(Stocks newShare) {
+
+    }
+
+    public void getStockDetails() {
+
+    }
+
+    public void calculateShareValue() {
+
+    }
+
+    public void removeStock(String shareName) {
+
+    }
+}
